8b306a9760313cdfe40db6525462956d9ee4f033
carlosavellar <carlos.avellar@gmail.com>
2022-04-15 17:25:42 -0300

    context

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,22 @@
 import React from "react";
-import Pizza from "./Pizza";
 import pizzas from "../data/pizzas.json";
-import AppCss from "./App.module.css";
-import LogoSvg from "./../assets/svg/pizza.svg";
+import Pizza from "./Pizza";
 import Cart from "./Cart";
+import AppCSS from "./App.module.css";
+import PizzaSVG from "../svg/pizza.svg";
 import AppStateProvider from "./AppState";
-
-const App: React.FC = () => {
+const App = () => {
   return (
     <AppStateProvider>
-      <div className={AppCss.container}>
-        <div className={AppCss.header}>
-          <LogoSvg width={120} height={120} />
-          <div className={AppCss.siteTitle}>Delicious Pizza</div>
+      <div className={AppCSS.container}>
+        <div className={AppCSS.header}>
+          <PizzaSVG width={120} height={120} />
+          <div className={AppCSS.siteTitle}>Delicious Pizza</div>
           <Cart />
         </div>
         <ul>
           {pizzas.map((pizza) => {
-            return <Pizza pizza={pizza} key={pizza.id} />;
+            return <Pizza key={pizza.id} pizza={pizza} />;
           })}
         </ul>
       </div>
--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import PizzaCss from "./Pizza.module.css";
-
+import React from "react";
+import PizzaCSS from "./Pizza.module.css";
+import { useSetState } from "./AppState";
 interface Pizza {
   id: number;
   name: string;
@@ -13,11 +13,40 @@ interface Props {
 }
 
 const Pizza: React.FC<Props> = ({ pizza }) => {
+  const setState = useSetState();
+  const handlerClickAddItem = () => {
+    setState((state) => {
+      const itemExist = state.cart.items.find((item) => item.id === pizza.id);
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          items: itemExist
+            ? state.cart.items.map((item) => {
+                if (item.id === pizza.id) {
+                  return { ...item, quantity: item.quantity + 1 };
+                }
+                return item;
+              })
+            : [
+                ...state.cart.items,
+                {
+                  id: pizza.id,
+                  name: pizza.name,
+                  price: pizza.price,
+                  quantity: 1,
+                },
+              ],
+        },
+      };
+    });
+  };
   return (
-    <li className={PizzaCss.container}>
+    <li className={PizzaCSS.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
-      <strong>{pizza.price}</strong>
+      <p>{pizza.price}</p>
+      <button onClick={handlerClickAddItem}>add</button>
     </li>
   );
 };
