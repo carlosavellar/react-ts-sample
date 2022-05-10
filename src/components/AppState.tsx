6e1851572536814f44f4d2f7ce2fb339eb58f4ff
carlos.avellar <carlos.avellar@gmail.com>
2022-05-09 22:32:59 -0300

    Ok

--- src/components/AppState.tsx
+++ src/components/AppState.tsx
@@ -1,8 +1,14 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useReducer, useState } from "react";
 
+interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+}
 interface AppStateValue {
   cart: {
-    items: { id: number; name: string; price: number; quantity: number }[];
+    items: CartItem[];
   };
 }
 
@@ -14,26 +20,68 @@ const defaultStateValue: AppStateValue = {
 
 export const AppStateContext = createContext(defaultStateValue);
 
-export const AppSetStateContext = createContext<
-  React.Dispatch<React.SetStateAction<AppStateValue>> | undefined
+export const AppDispatchStateContext = createContext<
+  React.Dispatch<AddItemAction> | undefined
 >(undefined);
 
-export const useSetState = () => {
-  const setState = useContext(AppSetStateContext);
-  if (!setState) {
-    throw new Error("NO setState in this context");
+export const useDispatchState = () => {
+  const dispatch = useContext(AppDispatchStateContext);
+  if (!dispatch) {
+    throw new Error("NO dispatch in this context");
   }
-  return setState;
+  return dispatch;
+};
+
+interface Action<T> {
+  type: T;
+}
+
+interface AddItemAction extends Action<"ADD_ITEM"> {
+  payload: {
+    item: CartItem;
+  };
+}
+
+const reducer = (state: AppStateValue, action: AddItemAction) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      const itemToAdd = action.payload.item;
+      const itemExist = state.cart.items.find(
+        (item) => item.id === itemToAdd.id
+      );
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          items: itemExist
+            ? state.cart.items.map((item) => {
+                if (item.id === itemExist.id) {
+                  return { ...item, quantity: item.quantity + 1 };
+                }
+                return item;
+              })
+            : [
+                ...state.cart.items,
+                {
+                  id: itemToAdd.id,
+                  name: itemToAdd.name,
+                  price: itemToAdd.price,
+                  quantity: 1,
+                },
+              ],
+        },
+      };
+  }
 };
 
 const AppContextProvider: React.FC = ({ children }) => {
-  const [state, setState] = useState(defaultStateValue);
-
+  // const [state, setState] = useState(defaultStateValue);
+  const [state, dispatch] = useReducer(reducer, defaultStateValue);
   return (
     <AppStateContext.Provider value={state}>
-      <AppSetStateContext.Provider value={setState}>
+      <AppDispatchStateContext.Provider value={dispatch}>
         {children}
-      </AppSetStateContext.Provider>
+      </AppDispatchStateContext.Provider>
     </AppStateContext.Provider>
   );
 };
